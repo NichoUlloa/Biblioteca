bca0ca2b2e1f8c5c3adefc8084e7b57fd01a23a1
NichoUlloa <n.ulloa06@ufromail.cl>
2023-10-23 14:49:00 -0300

    se agregaron nuevos metodos y atributo de ISBN.

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 public class Biblioteca {
+
     private ArrayList<Libro> listaLibros;
     private String nombreBiblioteca;
     private String direccionBiblioteca;
